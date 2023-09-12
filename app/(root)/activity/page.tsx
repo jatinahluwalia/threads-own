cb1cbd4c35670aadfc8bce157b7f650b02517da0
jitu2404 <103820854+jitu2404@users.noreply.github.com>
2023-09-12 21:28:23 +0530

    to be deployed...

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -27,6 +27,12 @@ const Activity = async () => {
                     height={20}
                     className="rounded-full object-cover"
                   />
+                  <p className="!text-small-regular text-light-1">
+                    <span className="mr-1 text-primary-500">
+                      {activity.author.name}
+                    </span>{" "}
+                    replied to your thread
+                  </p>
                 </article>
               </Link>
             ))}
