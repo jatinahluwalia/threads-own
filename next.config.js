e3db3cb07bf3cb93edebadaa4a18e1d3cdbb53c2
jitu2404 <103820854+jitu2404@users.noreply.github.com>
2023-09-12 21:38:34 +0530

    minify the code

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  swcMinify: true,
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
